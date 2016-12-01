53090a8b11afc472fac37d64923ff210c5a0cdb8
Shelley Wong <swong26@mail.csuchico.edu>
2016-11-30 16:24:54 -0800

    add functionality to recall previously saved grocery lists

--- groceries.js
+++ groceries.js
@@ -1,43 +1,10 @@
 var myList = [];
+window.onload = loadCookieList();  //when the window loads, do this function
 
 function addItem(){
   //Get the value of what the user typed in the text box and assign it to a variable called input
   var input = document.getElementById("newItem").value;
-  //if the item is not already in the list, add it to the list
-  if(myList.indexOf(input) == -1) {
-    //add item to myList array
-    myList.push(input);
-    console.log(myList);
-    //Get the element listDisplay and assign it to a variable called list
-    var list = document.getElementById("listDisplay");
-    //Create a new variable called item and assign it to: document.createElement("li");
-    //This creates a new list item element for the document object model (DOM).
-    var item = document.createElement("li");
-    //Create a new variable called itemName and assign it to: document.createTextNode(input);
-    //This creates plain text element from the value of the variable input to put in the DOM.
-    var itemName = document.createTextNode(input);
-    //create a variable which contains a new button element
-    var btnClose = document.createElement("button");
-    btnClose.classList.add("button");
-    btnClose.classList.add("btn-danger");
-    btnClose.classList.add("btn-xs");
-    var iconClose = document.createElement("span");
-    iconClose.classList.add("glyphicon");
-    iconClose.classList.add("glyphicon-remove");
-    //when button is clicked, parent list item is removed
-    btnClose.addEventListener("click",removeParentListItem);
-    //make the span element a child to the button element
-    btnClose.appendChild(iconClose);
-    //This puts that plain text element as the innerHTML (child) of the li element called item
-    item.appendChild(itemName);
-    //make the btn element a child of the list element
-    item.appendChild(btnClose);
-    //This puts the li element (item) inside the ul in the document (list) so it can be seen on the screen
-    list.appendChild(item);
-    //Set the value attribute of the text input element to be an empty string so that
-    //what the user typed in is no longer shown in the text box
-    document.getElementById("newItem").value = "";
-  }
+  displayItem(input);
 }
 
 function removeParentListItem() {
@@ -62,6 +29,52 @@ function saveList(){
 function clearList(){
   document.getElementById("listDisplay").innerHTML = "";
   myList = [];
+}
+
+function displayItem(userInput){
+  //if the item is not already in the list, add it to the list
+  if(myList.indexOf(userInput) == -1) {
+    //add item to myList array
+    myList.push(userInput);
+    console.log(myList);
+    //Get the element listDisplay and assign it to a variable called list
+    var list = document.getElementById("listDisplay");
+    //Create a new variable called item and assign it to: document.createElement("li");
+    //This creates a new list item element for the document object model (DOM).
+    var item = document.createElement("li");
+    //Create a new variable called itemName and assign it to: document.createTextNode(input);
+    //This creates plain text element from the value of the variable input to put in the DOM.
+    var itemName = document.createTextNode(userInput);
+    //create a variable which contains a new button element
+    var btnClose = document.createElement("button");
+    btnClose.classList.add("button");
+    btnClose.classList.add("btn-danger");
+    btnClose.classList.add("btn-xs");
+    var iconClose = document.createElement("span");
+    iconClose.classList.add("glyphicon");
+    iconClose.classList.add("glyphicon-remove");
+    //when button is clicked, parent list item is removed
+    btnClose.addEventListener("click",removeParentListItem);
+    //make the span element a child to the button element
+    btnClose.appendChild(iconClose);
+    //This puts that plain text element as the innerHTML (child) of the li element called item
+    item.appendChild(itemName);
+    //make the btn element a child of the list element
+    item.appendChild(btnClose);
+    //This puts the li element (item) inside the ul in the document (list) so it can be seen on the screen
+    list.appendChild(item);
+    //Set the value attribute of the text input element to be an empty string so that
+    //what the user typed in is no longer shown in the text box
+    document.getElementById("newItem").value = "";
+  }
+}
+
+function loadCookieList(){
+  var groceryList = getCookie("groceryItems");
+  var arrayCookie = groceryList.split(" ");
+  for(var i=0; i<arrayCookie.length; i++){
+    displayItem(arrayCookie[i]);
+  }
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
